69bc2351fedb10e81d097ee4e38d42423c379a36
Eric Rivera <chocoverdose81@gmail.com>
2020-05-07 21:33:47 -0500

    This is my first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// let name = 'Eric';
+// var name2 = 'Eric';
+const name = 'Eric';
+
+const array = ['Darius', 'Eric', 'Ben', 'Mike'];
+
+console.log('this is only Ben'), array[2];
+
+for(let i = 0; i < array.length; i++) {
+    console.log(array[i])
+}
+
+// console.log(name);
+// console.log(name2);
+// console.log(name);
